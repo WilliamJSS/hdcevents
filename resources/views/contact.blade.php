b905de37f059c5cb814c83f18fc9b8b2c3972325
WilliamJSS <williamsena088@gmail.com>
2022-09-05 10:56:43 -0300

    feat: layouts e arquivos estáticos

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'HDC Events | Contatos')
+
+@section('content')
+    <h1>Essa é a página de contato</h1>
+    <a href="/">Voltar para Home</a>
+@endsection
---
+++ resources/views/products.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.main')
+
+@section('title', 'HDC Events | Produtos')
+
+@section('content')
+    <h1>Tela de Produtos</h1>
+    <a href="/">Voltar para Home</a>
+@endsection
